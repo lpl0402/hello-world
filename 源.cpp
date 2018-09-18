27ace486718e5586d897895b7e7e6f46eb76f66c
lpl0402 <32223055+lpl0402@users.noreply.github.com>
2018-09-18 09:26:36 +0800

    Client端

--- 源.cpp
+++ 源.cpp
@@ -2,16 +2,14 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<iostream>
+#include<string.h>
 using namespace std;
 #pragma comment(lib,"ws2_32.lib")
-SOCKET sockSer, sockCli;
-SOCKADDR_IN addSer, addCli;
+SOCKET sockSer,sockCli;
+SOCKADDR_IN addSer,addCli;
 const int buf_len = 2048;
 char input[buf_len];
 char recvbuf[buf_len];
-
-
-
 int main()
 {
 	WSADATA data;
@@ -22,75 +20,37 @@ int main()
 		system("pause");
 		return 0;
 	}
-	sockSer = socket(AF_INET, SOCK_STREAM, 0);//af(address fimaly)inet(ipv4),sock_stream(tcp)
-	addSer.sin_family = AF_INET;//sin(socket in)
-	addSer.sin_addr.S_un.S_addr = INADDR_ANY;
-	addSer.sin_port = htons(12345);
-	bind(sockSer, (sockaddr *)&addSer, sizeof(addSer));
+	sockCli = socket(AF_INET, SOCK_STREAM, 0);//af(address fimaly)inet(ipv4),sock_stream(tcp)
+	addCli.sin_family = AF_INET;//sin(socket in)
+	addCli.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
+	addCli.sin_port = htons(12345);
 
 
+	//sockSer = socket(AF_INET, SOCK_STREAM, 0);//af(address fimaly)inet(ipv4),sock_stream(tcp)
+	//addSer.sin_family = AF_INET;//sin(socket in)
+	//addSer.sin_addr.S_un.S_addr = INADDR_ANY;
+	//addSer.sin_port = htons(12345);
 
-	//listen(sockSer, 5);//3-5
-	//cout << "#####服务端已启动#####" << endl;
-	//sockCli = accept(sockSer, (sockaddr *)&addCli, &size);
-	//if (sockCli != INVALID_SOCKET)  //也可用NULL判断
-	//{
-	//	printf("IP地址为%s的服务器已连接\n", inet_ntoa(addCli.sin_addr));
-	//	send(sockCli, "您已成功与服务器连接", sizeof("您已成功与服务器连接"), 0);
-	//}
-
-	while(1)
+	if (connect(sockCli, (sockaddr *)&addCli, size) != SOCKET_ERROR)
 	{
-		listen(sockSer, 5);//3-5
-		cout << "#####服务端已启动#####" << endl;
-		sockCli = accept(sockSer, (sockaddr *)&addCli, &size);
-		if (sockCli != INVALID_SOCKET)  //也可用NULL判断
+		while (1)
 		{
-			printf("IP地址为%s的服务器已连接\n", inet_ntoa(addCli.sin_addr));
-			send(sockCli, "您已成功与服务器连接", sizeof("您已成功与服务器连接"), 0);
-			while (1)
+			memset(recvbuf, buf_len, sizeof(recvbuf));
+			recv(sockCli, recvbuf, sizeof(recvbuf), 0);
+			cout << "Server:";
+			cout << recvbuf << endl << "Client:";
+			cin >> input;
+			send(sockCli, input, strlen(input)+1, 0);
+			if (strcmp(input, "exit") == 0)
 			{
-				recv(sockCli, recvbuf, sizeof(recvbuf), 0);
-				if(strlen(recvbuf) != 0)
-				{ 
-
-				    cout << "Client:";
-					cout << recvbuf << endl;
-					if (strcmp(recvbuf, "exit") == 0)
-					{
-						cout << "客户端已退出" << endl;
-						break;
-						//return 0;
-					}
-					cout << "Server:";
-					cin >> input;
-				    send(sockCli, input, strlen(input)+1, 0);
-				    if (strcmp(input, "exit") == 0 )
-				    {
-					    break;
-					   //return 0;
-				    }
-				}
-
+				break;
 			}
 		}
-
-	/*	sockCli = accept(sockSer, (sockaddr *)&addCli, &size);
-		recv(sockSer, recvbuf, sizeof(recvbuf), 0);
-		cout << "Client:";
-		cout << recvbuf << endl << "Server:";
-		cin >> input;
-		send(sockCli, input, strlen(input), 0);
-		if (strcmp(input, "exit") == 0)
-		{
-			return 0;
-		}*/
-
 	}
-    
-	closesocket(sockSer);
+	cout << "感谢本次连接" << endl;
 	closesocket(sockCli);
 	WSACleanup();
 	system("pause");
 	return 0;
+
 }
